e9ebc43087221228d13f93586555ba2273c00f0b
Gabriel Zarate <gzaratet1@gmail.com>
2019-10-09 10:26:34 -0500

    React a PlatziVideo

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,36 +5,44 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
-    <Categories title='Mi Lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+const API = 'http://localhost:3000/initalState/';
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+const App = () => {
+  const initialState = useInitialState(API);
+  return initialState.length === 0 ? <h1>Loading...</h1> : (
+    <div className='App'>
+      <Header />
+      <Search />
+      {
+        ((initialState.mylist.length > 0) &&
+          (
+            <Categories title='Mi Lista'>
+              <Carousel>
+                {initialState.mylist.map((item) => <CarouselItem key={item.id} {...item}/>)}
+              </Carousel>
+            </Categories>
+          )
+        )
+      }
 
-    <Categories title='Originales de Platzi Video'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+      <Categories title='Tendencias'>
+        <Carousel>
+          {initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+      <Categories title='Originales de Platzi Video'>
+        <Carousel>
+          {initialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+          
+        </Carousel>
+      </Categories>
 
-    <Footer />
-  </div>
-);
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
